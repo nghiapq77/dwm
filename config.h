d8e5b58300076fa5afc1714f91d20c454dcf38e1
Nghia Pham <nghia.pq77@gmail.com>
2024-08-23 09:18:06 +0700

    Add window rules

--- config.h
+++ config.h
@@ -39,9 +39,13 @@ static Sp scratchpads[] = {
 static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
-    /* class    instance     title                    tags mask    isfloating    monitor */
-    { NULL,     "spterm",    NULL,                    SPTAG(0),    1,            -1 },
-    { NULL,     NULL,        "Picture-in-Picture",    0,           1,            -1 },
+    /* class              instance     title                    tags mask    isfloating    monitor */
+    { "LibreWolf",        NULL,        NULL,                    1,           0,            -1 },
+    { "st-256color",      "tm",        NULL,                    1 << 1,      0,            -1 },
+    { "Brave-browser",    NULL,        NULL,                    1 << 2,      0,            -1 },
+    { "st-256color",      "lf",        NULL,                    1 << 3,      0,            -1 },
+    { NULL,               "spterm",    NULL,                    SPTAG(0),    1,            -1 },
+    { NULL,               NULL,        "Picture-in-Picture",    0,           1,            -1 },
 };
 
 /* layout(s) */
